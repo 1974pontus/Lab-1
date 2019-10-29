2d267f27683cb31e9060a58f0edd6a74ebc7f515
Pontus Tahir <pontus.tahir@gmail.com>
2019-10-29 09:06:00 +0100

    hide button

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-const textElement = document.querySelector('text')
-const optionButtonsElement = document.querySelector('options-button')
+function myFunction() {
+    var person = prompt("Ditt namn, tack");
+    if (person != null) {
+        document.getElementById("playerName").innerHTML =
+            "Hej " + person + "! Välkommen till det fetaste spelet!";
+    }
+}
+
+
 
